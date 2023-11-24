803e8bac9aa2ee2d886cfa4075f403dae25fb60e
Adarsh4775 <137145969+Adarsh4775@users.noreply.github.com>
2023-11-24 20:13:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 function createTable() {
     //Write your code here
-  
+  let inputRows = parseFloat(prompt("Input number of rows"));
+	let rn = inputRows.value;
+	let inputColumns = parseFloat(prompt("Input number of columns"));
+	let cn = inputColumns.value;
+
+	let table = document.getElementById("myTable");
+
+	let tableRow = document.createElement("tr");
+		let tableColumn = document.createElement("td");
+	     tableColumn.innerText = "Row-" + rn + "Column-" + cn ;
+         tableRow.appendChild(tableColumn);
+	      tableRow.appendChild(table);
 }
