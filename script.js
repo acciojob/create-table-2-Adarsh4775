fe8c1aa9a6d10e5a4b18212447b77f6b59637324
Adarsh4775 <137145969+Adarsh4775@users.noreply.github.com>
2023-11-27 16:14:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,31 @@
 function createTable() {
-    //Write your code here
-  let inputRows = parseFloat(prompt("Input number of rows"));
-	let rn = inputRows.value;
-	let inputColumns = parseFloat(prompt("Input number of columns"));
-	let cn = inputColumns.value;
+    let inputRows = parseFloat(prompt("Input number of rows"));
+    let inputColumns = parseFloat(prompt("Input number of columns"));
 
-	let table = document.getElementById("myTable");
+    // Check if the inputs are valid numbers
+    if (isNaN(inputRows) || isNaN(inputColumns) || inputRows <= 0 || inputColumns <= 0) {
+        alert("Invalid input. Please enter valid numbers for rows and columns.");
+        return;
+    }
 
-	let tableRow = document.createElement("tr");
-		let tableColumn = document.createElement("td");
-	     tableColumn.innerText = "Row-" + 0 + "Column-" + 0 ;
-         tableRow.appendChild(tableColumn);
-	      table.appendChild(tableRow);
+    let rn = Math.floor(inputRows); // Ensure a whole number of rows
+    let cn = Math.floor(inputColumns); // Ensure a whole number of columns
+
+    let table = document.getElementById("myTable");
+
+    // Clear existing table content
+    table.innerHTML = '';
+
+    // Create rows and columns based on user input
+    for (let i = 0; i < rn; i++) {
+        let tableRow = document.createElement("tr");
+
+        for (let j = 0; j < cn; j++) {
+            let tableColumn = document.createElement("td");
+            tableColumn.innerText = "Row-" + i + " Column-" + j;
+            tableRow.appendChild(tableColumn);
+        }
+
+        table.appendChild(tableRow);
+    }
 }
